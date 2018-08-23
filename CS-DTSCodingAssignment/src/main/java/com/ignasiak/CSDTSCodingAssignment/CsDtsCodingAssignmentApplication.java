6b7b10a0ee02d141313306a82cf0e86a6fb55178
piotrign <31850105+piotrign@users.noreply.github.com>
2018-08-23 23:07:27 +0200

    Project initialization

---
+++ CS-DTSCodingAssignment/src/main/java/com/ignasiak/CSDTSCodingAssignment/CsDtsCodingAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.ignasiak.CSDTSCodingAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsDtsCodingAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsDtsCodingAssignmentApplication.class, args);
+	}
+}
